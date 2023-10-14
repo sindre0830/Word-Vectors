ad5d88a3f0827792e94d2cb27bdec9c72f6de728
Sindre Eiklid <sindre0830@gmail.com>
2023-10-14 23:18:04 +0200

    Set seed in CBOW program

--- source/architechtures/cbow.py
+++ source/architechtures/cbow.py
@@ -4,8 +4,12 @@ from constants import (
 import utils
 
 import os
+import numpy as np
+import torch
 
 
 def run() -> None:
     config = utils.load_config(filepath=os.path.join(PROJECT_DIRECTORY_PATH, "source", "config_cbow.yml"))
-    print(config.seed)
+    # set seed
+    np.random.seed(config.seed)
+    torch.manual_seed(config.seed)
