3b3dbfccbd10feb5f74e47fbc29f9c3b742e542e
Sindre Eiklid <sindre0830@gmail.com>
2023-10-14 23:30:53 +0200

    Add corpus loader download step

--- source/datahandler/loaders.py
+++ source/datahandler/loaders.py
@@ -1,4 +1,12 @@
+import utils
+
+import gensim.downloader
+
+
 class CorpusLoader():
     def __init__(self) -> None:
         self.corpus = None
         self.is_flattened = False
+
+    def download(self) -> None:
+        self.corpus = gensim.downloader.load("text8")
