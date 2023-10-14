f5d44b8868847529f645e7970bdc5c424fbea658
Sindre Eiklid <sindre0830@gmail.com>
2023-10-14 22:38:41 +0200

    Load CBOW config file

--- source/architechtures/cbow.py
+++ source/architechtures/cbow.py
@@ -1,2 +1,11 @@
+from constants import (
+    PROJECT_DIRECTORY_PATH
+)
+import utils
+
+import os
+
+
 def run() -> None:
-    pass
+    config = utils.load_config(filepath=os.path.join(PROJECT_DIRECTORY_PATH, "source", "config_cbow.yml"))
+    print(config.seed)
